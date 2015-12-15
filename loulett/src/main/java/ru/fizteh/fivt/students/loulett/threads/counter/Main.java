9dfd6d9950876adae8f372399227b984c9fe954e
Alexandra <aiglina@gmail.com>
2015-12-15 23:51:08 +0300

    Add Counter

---
+++ loulett/src/main/java/ru/fizteh/fivt/students/loulett/threads/counter/Main.java
@@ -0,0 +1,10 @@
+package ru.fizteh.fivt.students.loulett.threads.counter;
+
+/**
+ * Created by Sasha on 15.12.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Counter(Integer.parseInt(args[1]));
+    }
+}
