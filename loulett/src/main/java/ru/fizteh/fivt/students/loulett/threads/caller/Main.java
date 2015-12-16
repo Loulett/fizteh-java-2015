202859524251076c5ef09bfaf2e8b2380a6f0b00
Alexandra <aiglina@gmail.com>
2015-12-17 00:05:15 +0300

    Add caller

---
+++ loulett/src/main/java/ru/fizteh/fivt/students/loulett/threads/caller/Main.java
@@ -0,0 +1,10 @@
+package ru.fizteh.fivt.students.loulett.threads.caller;
+
+/**
+ * Created by Sasha on 16.12.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Caller(Integer.parseInt(args[1]));
+    }
+}
